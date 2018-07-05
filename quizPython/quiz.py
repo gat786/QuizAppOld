b52d9af81994d36074cecca82f2a9a845f7bf235
gat786 <ganesht049@gmail.com>
2018-07-05 18:02:09 +0530

    python file added

--- quizPython/quiz.py
+++ quizPython/quiz.py
@@ -54,7 +54,6 @@ def _storeInTable(table_name,data):
     mycursor.execute("create table "+table_name+"(question varchar(100), option1 varchar(50), option2 varchar(50),option3 varchar(50),answer varchar(50))")
     listQuestions=parseJsonToList(data)
     for a in listQuestions:
-        print(a)
         question=html.unescape(a["question"])
         option1=html.unescape( a["option1"])
         option2=html.unescape( a["option2"])
@@ -71,24 +70,30 @@ def saveDataToDatabase(data,database_name,table_name):
     
     #Check if database exists if exists then use else create 
     if checkExistence(database_name,"databases"):
+        #database exists using the same
         mycursor.execute("use "+database_name)
         if checkExistence(table_name,"tables"):
+            #table exists dropping table
             mycursor.execute("drop table "+table_name)
+            #new table creating and saving data
             _storeInTable(table_name,data)
         else:
+            #new table creating and saving data
             _storeInTable(table_name,data)        
     else:
+        #database creating 
         mycursor.execute("create database "+database_name)
+        #new table creating and saving data
         _storeInTable(table_name,data)
 
 
 
 
-saveDataToDatabase(getDataFromUrl("17","hard","46","multiple"),"trivia_db","science_hard_multiple")
-saveDataToDatabase(getDataFromUrl("22","hard","43","multiple"),"trivia_db","geography_hard_multiple")
-saveDataToDatabase(getDataFromUrl("23","hard","50","multiple"),"trivia_db","history_hard_multiple")
-saveDataToDatabase(getDataFromUrl("20","hard","8","multiple"),"trivia_db","mythology_hard_multiple")
-saveDataToDatabase(getDataFromUrl("11","hard","29","multiple"),"trivia_db","films_hard_multiple")
+saveDataToDatabase(getDataFromUrl("17","hard","41","multiple"),"trivia_db","science_hard_multiple")
+saveDataToDatabase(getDataFromUrl("22","hard","42","multiple"),"trivia_db","geography_hard_multiple")
+saveDataToDatabase(getDataFromUrl("23","hard","46","multiple"),"trivia_db","history_hard_multiple")
+saveDataToDatabase(getDataFromUrl("20","hard","7","multiple"),"trivia_db","mythology_hard_multiple")
+saveDataToDatabase(getDataFromUrl("11","hard","28","multiple"),"trivia_db","films_hard_multiple")
 saveDataToDatabase(getDataFromUrl("21","hard","9","multiple"),"trivia_db","sports_hard_multiple")
-saveDataToDatabase(getDataFromUrl("18","hard","24","multiple"),"trivia_db","computers_hard_multiple")
+saveDataToDatabase(getDataFromUrl("18","hard","22","multiple"),"trivia_db","computers_hard_multiple")
 
